3d1bb965c072257dc4eef9e7bd839e67f804b581
Andrew-Goit <105640738+Andrew-Goit@users.noreply.github.com>
2023-03-18 01:09:57 +0200

    hw-05 is done

--- controllers/auth.js
+++ controllers/auth.js
@@ -2,10 +2,15 @@ const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 const {User} = require("../models/user");
 const {HttpError, ctrlWrapper} = require("../helpers");
+const gravatar = require('gravatar');
+const path = require("path");
+const fs = require("fs/promises");
+var Jimp = require("jimp");
 
 require("dotenv").config();
 
 const {SECRET_KEY} = process.env;
+const avatarsDir = path.join(__dirname, "../", "public", "avatars");
 
 const register = async(req, res) => {
     const {email, password} = req.body;
@@ -14,7 +19,8 @@ const register = async(req, res) => {
         throw HttpError(409, "This email is already in use");
     }
     const hashPassword = await bcrypt.hash(password, 10);
-    const newUser = await User.create({...req.body, password: hashPassword});
+    const avatarURL = gravatar.url(email);
+    const newUser = await User.create({...req.body, password: hashPassword, avatarURL});
     res.status(201).json({
         email: newUser.email,
         name: newUser.name,
@@ -34,7 +40,6 @@ const login = async(req,res) => {
     }
     const payload = {
         id: user._id,
-        // id: "64071b1c74bdac186e04d7f1"
     }
     const token = jwt.sign(payload, SECRET_KEY, {expiresIn: "30d"});
     await User.findByIdAndUpdate(user._id, { token });
@@ -68,13 +73,27 @@ const updateSubscription = async (req, res) => {
     });
 }
 
+const updateAvatar = async (req,res) => {
+    const {_id} = req.user;
+    const {path: tempUpload, originalname} = req.file;
+    const filename = `${_id}_${originalname}`;
+    const resultUpload = path.join(avatarsDir, filename);
+    await fs.rename(tempUpload, resultUpload);
+    const avatarURL = path.join("avatars", filename);
+    Jimp.read(resultUpload, (err, avatar) => {
+        if (err) throw err;
+          avatar.resize(250, 250).write(resultUpload);
+      });
+    await User.findByIdAndUpdate(_id, {avatarURL});
+    res.json({avatarURL});
+}
+
 const logout = async (req, res) => {
     const {_id} = req.user;
     await User.findByIdAndUpdate(_id, {token: ""});
     res.status(204).json({
         message: "Logout success"
     })
-
 }
 
 module.exports = {
@@ -83,4 +102,5 @@ module.exports = {
     getCurrent: ctrlWrapper(getCurrent),
     updateSubscription: ctrlWrapper(updateSubscription),
     logout: ctrlWrapper(logout),
+    updateAvatar: ctrlWrapper(updateAvatar),
 }
